d75e39322dcf6936c7f27ca351b4d5e12906efab
Cecilia <109989926+Eon4@users.noreply.github.com>
2023-09-07 09:14:02 +0200

    update

---
+++ toast/src/components/Toast/toast.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import style from '../Toast/toast.module.scss'
+
+export function Toast(){
+    return (
+        <h1 className={style.title}>Lets build a toast!</h1>
+    )
+}
